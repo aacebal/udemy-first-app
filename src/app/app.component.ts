3e216940edf21d93b19c984dc865ef9930a0f632
Adel Acebal <adelacebal@yahoo.com>
2018-01-08 20:14:25 -0500

    adding components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServersComponent } from './servers/servers.component';
 
 @Component({
   selector: 'app-root',
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ServerComponent } from './server/server.component';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
