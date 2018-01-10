236cf958c7fe16864113d9f6d6093334d675e824
Adel Acebal <adelacebal@yahoo.com>
2018-01-09 21:18:59 -0500

    ng style

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,11 +10,17 @@ export class ServerComponent implements OnInit {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
-  getServerStatur() {
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerStatus() {
     return this.serverStatus;
   }
 
-  constructor() { }
+  getColor() {
+    return this.serverStatus === 'online' ?'green' : 'red';
+  }
 
   ngOnInit() {
   }
